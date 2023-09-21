a15dfc3f1eb72ff087f46edf438b2b470b1cd8c3
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2023-09-20 20:05:54 -0500

    Update wall_sensing.py

--- wall_sensing.py
+++ wall_sensing.py
@@ -1,11 +1,5 @@
 """
 Rush the robot to a wall, stop at the required distance
 """
-from gpiozero import DistanceSensor
-from time import sleep
-
-# sensor = DistanceSensor(23, 24)
-
-# while True:
-    # print('Distance to nearest object is', sensor.distance, 'm')
-    # sleep(1)
+# from machine import Pin, PWM
+from picozero import DistanceSensor
